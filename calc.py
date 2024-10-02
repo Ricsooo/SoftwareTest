f64b7df55c06162f0626bbf2f7aa318ef5f35bcd
Nanasi Richard <nanasi.richard@diak.szbi-pg.hu>
2024-10-02 08:45:22 +0200

    számolo

---
+++ calc.py
@@ -0,0 +1,10 @@
+def add (x,y):
+    return x+y
+def substract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    if y == 0:
+        return "nullával nem oszt"
+    return x/y
